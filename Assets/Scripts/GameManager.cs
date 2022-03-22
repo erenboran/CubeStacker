16664f2cdb4fe9ae1bb9318b6a902f6102fd2e3c
plejae <erenn.borann@gmail.com>
2022-03-23 00:08:49 +0300

    Game
    Some basic mechanic prefabs and enums

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static event Action OnCubeSpawned = delegate { };
+
+    private CubeSpawner[] spawners;
+    private int spawnerIndex;
+    private CubeSpawner currentSpawner;
+
+    private void Awake()
+    {
+        spawners = FindObjectsOfType<CubeSpawner>();
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (MovingCube.CurrentCube != null) //&& MovingCube.LastCube.gameObject != GameObject.Find("Start"))
+            MovingCube.CurrentCube.Stop();
+
+            spawnerIndex = spawnerIndex == 0 ? 1 : 0;
+            currentSpawner = spawners[spawnerIndex];
+
+            currentSpawner.SpawnCube();
+            OnCubeSpawned();
+        }
+    }
+}
